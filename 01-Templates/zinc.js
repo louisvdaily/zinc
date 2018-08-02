5fbd1bbc72300dbd6cec38168004928cdcf9cf05
louisdailygalvanize <louisvdaily@gmail.com>
2018-08-02 13:32:25 -0500

    Completed creating elements with templates

--- 01-Templates/zinc.js
+++ 01-Templates/zinc.js
@@ -5,28 +5,30 @@
 (() => {
     function populateList(results) {
         console.log(results); // eslint-disable-line no-console
+
+        let template = `
+        <li class="user">
+            <img class="user-photo" src="{{ photo }}" alt="Photo of {{ firstName }} {{ lastName }}">
+            <div class="user-name">{{ firstName }} {{ lastName }}</div>
+            <div class="user-location">{{ city }}, {{ state }}</div>
+            <div class="user-email">{{ email }}</div>
+        </li>
+        `
+
         // Fetch user list element from DOM first
         let userList = document.getElementById('z-user-list');
 
-        // Then populate list with content from results array.
         for (let userIndex = 0; userIndex < results.length; userIndex++) {
             let user = results[userIndex];
-
-            let photo = user.picture.thumbnail;
-            let firstName = capitalize(user.name.first);
-            let lastName = capitalize(user.name.last);
-            let city = capitalize(user.location.city);
-            let state = capitalize(user.location.state);
-            let email = user.email;
-
-            userList.insertAdjacentHTML('beforeend', `
-                <li class="user">
-                    <img class="user-photo" src="${ photo }" alt="Photo of ${ firstName } ${ lastName }">
-                    <div class="user-name">${ firstName } ${ lastName }</div>
-                    <div class="user-location">${ city }, ${ state }</div>
-                    <div class="user-email">${ email }</div>
-                </li>
-            `);
+            let userData = {
+                photo: user.picture.thumbnail,
+                firstName: capitalize(user.name.first),
+                lastName: capitalize(user.name.last),
+                city: capitalize(user.location.city),
+                state: capitalize(user.location.state),
+                email: user.email
+            }
+            userList.insertAdjacentHTML('beforeend', renderTemplate(template, userData));
         }
     }
 
@@ -34,6 +36,10 @@
         let words = string.split(' ');
         let capitalWords = words.map(word => word[0].toUpperCase() + word.slice(1));
         return capitalWords.join(' ');
+    }
+
+    function renderTemplate(templateString, object) {
+        return templateString.replace(/{{\s*(\w+)\s*}}/g, (match, captured) => object[captured]);
     }
 
     function init() {
